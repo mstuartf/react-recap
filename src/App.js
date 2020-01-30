47beb53aa7be5fb6bb3e190112a8a0c60dbf87fc
mikef <mstuartflood@gmail.com>
2020-01-30 20:53:31 +0000

    force redirect to characters

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router';
+import { Route, Switch, Redirect } from 'react-router';
 import CharacterList from './components/CharacterList';
 import CharacterDetail from './components/CharacterDetail';
 
@@ -8,9 +8,9 @@ const App = () => {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/"/>
         <Route exact path="/characters" component={CharacterList}/>
         <Route path="/characters/:id" component={CharacterDetail}/>
+        <Route path="*" render={() => <Redirect to="/characters"/>}/>
       </Switch>
     </div>
   );
